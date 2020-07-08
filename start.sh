4717481a8c0aa24776d1da0e842a8645ed7d0973
victor-chunplam <victor.chunplam@gmail.com>
2020-07-08 17:10:17 +0800

    Add independent JDK

--- start.sh
+++ start.sh
@@ -15,7 +15,7 @@ for ((i = 0; IS_STOP < 1 && i < RETRY_TIMES; i++)); do
   else
     echo "MySQL is ready"
     echo "$NEW_MESSAGE"
-    ./gradlew flywayClean flywayBaseline flywayMigrate generateJooq
+    ./gradlew flywayClean flywayBaseline flywayMigrate generateJooq -Dorg.gradle.java.home="./jdk1.8.0_211.jdk/Contents/Home"
     IS_STOP=1
   fi
 
@@ -25,7 +25,7 @@ done
 if [ "$IS_STOP" = 1 ]; then
   echo "DB is ready! Enjoy!"
   echo "Starting Spring Boot application listening to port 8080 with TESTING......"
-  ./gradlew test bootRun
+  ./gradlew test bootRun -Dorg.gradle.java.home="./jdk1.8.0_211.jdk/Contents/Home"
 else
   echo "DB cannot start!"
 fi
